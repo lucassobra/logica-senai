92edcd259240960fc0f0bfb5a5e64af574dc638d
jlog180924 <jlog180924@info.local>
2018-09-25 22:07:29 -0300

    aula 2

---
+++ workspace/imperadorees-java/src/aritimeticos/soma.java
@@ -0,0 +1,10 @@
+package aritimeticos;
+
+public class soma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("E`hora de dar tchau");
+	}
+
+}
